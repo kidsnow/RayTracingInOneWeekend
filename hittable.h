12d28ff62987a5330bad51f90be45ae49e77410b
kidsnow <kidsnow0115@gmail.com>
2024-05-27 00:12:49 +0900

    chap10 finished

--- hittable.h
+++ hittable.h
@@ -1,10 +1,13 @@
 #pragma once
 
+class material;
+
 class hit_record
 {
 public:
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat;
     double t;
     bool front_face;
 
--- sphere.h
+++ sphere.h
@@ -5,12 +5,10 @@
 class sphere : public hittable
 {
 public:
-    sphere(const point3& center, double radius) :
+    sphere(const point3& center, double radius, shared_ptr<material> mat) :
         center(center),
-        radius(fmax(0, radius))
-    {
-        
-    }
+        radius(fmax(0, radius)),
+        mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override
     {
@@ -38,6 +36,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -45,4 +44,5 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
